494d8f2dcd38ddf6b572c64fd6a502b29344c6cb
VladislavBizin <vladislav.bizin@gmail.com>
2019-09-24 22:28:25 +0300

    feat: implement "decoder" function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const translator = new Map([
+        ["10","."],
+        ["11","-"],
+        ["**********", " "]
+    ]);
+
+    let lettersPolygon = [];
+    for(let i = 0, j = 10; j <= expr.length; i += 10, j += 10){
+        lettersPolygon = [...lettersPolygon, expr.slice(i,j)];
+    }
+
+    const toMorse = (str) => {
+        let result = '';
+        for(let i = 0, j = 2; j <= str.length; i += 2, j += 2){
+            if(translator.has(str.slice(i,j))){
+                result = result + translator.get(str.slice(i,j));
+            }
+        }
+        return MORSE_TABLE[result];
+    };
+
+    return  lettersPolygon.reduce((acc, elem) => {
+        if(translator.has(elem)){
+            return acc + translator.get(elem);
+        }
+        return acc + toMorse(elem);
+    },'');
 }
 
 module.exports = {
